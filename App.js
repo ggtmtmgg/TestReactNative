513ccb1ad533d43ab13ac7d2b1c70cb05dda2ead
Tnarita0000 <naritatakuya0000@gmail.com>
2018-08-05 16:17:41 +0900

    update App.js

--- App.js
+++ App.js
@@ -1,49 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from "react";
+import { AppRegistry, TextInput, ScrollView } from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class UselessTextInput extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { text: "Useless Placeholder" };
+	}
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+	render() {
+		return (
+      <ScrollView style={{ marginTop: 20 }}>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={text => this.setState({ text })}
+          value={this.state.text}
+        />
+        <TextInput
+          style={{ height: 200, borderColor: "gray", borderWidth: 1 }}
+          onChangeText={text => this.setState({ text })}
+          multiline={true}
+          numberOfLines={4}
+          value={this.state.text}
+        />
+      </ScrollView>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+AppRegistry.registerComponent("AwesomeProject", () => UselessTextInput);
