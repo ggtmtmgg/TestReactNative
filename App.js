b6e1e96664dfdae741dbaf2f513f8ed0d7f77121
ggtmtmgg <xxx.tubasa0226@gmail.com>
2018-09-21 09:23:18 +0900

    demo

--- App.js
+++ App.js
@@ -1,49 +1,55 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import * as React from 'react'
+import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export default class App extends React.Component {
+  inputRef = React.createRef()
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text>
+          How to reproduce:{'\n'}
+          1. Click the buttons below{'\n'}
+          2. See that it changes the font size as expected{'\n'}
+          3. Type something in the TextInput{'\n'}
+          4. Click the buttons again{'\n'}
+          5. See that it doesn't work anymore
+        </Text>
+
+        <TouchableOpacity
+          onPress={() => this.inputRef.current.setNativeProps({ style: { backgroundColor: "blue", fontSize: 20  } })}
+          style={{ margin: 20 }}
+        >
+          <Text>Set fontSize=20</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          onPress={() => { 
+            this.inputRef.current.setNativeProps({ style: { backgroundColor: "yellow", fontSize: 40 } });
+          }}
+          style={{ margin: 20 }}
+        >
+          <Text>Set fontSize=40</Text>
+        </TouchableOpacity>
+
+        <TextInput
+          ref={this.inputRef}
+          onChange={() => console.log(this.value)}
+          placeholder=""
+          value="iiii"
+          style={{ width: '100%', padding: 20, fontSize: 20, backgroundColor: 'white' }}
+        />
+
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    padding: 20,
+    paddingTop: 40,
+    backgroundColor: '#ecf0f1',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+})
